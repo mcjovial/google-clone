e1a0431b3ac9f5e213c2d6e219220a9aa9080e49
mcjovial <emmanuelelias455@gmail.com>
2021-08-16 02:03:14 +0100

    header section

---
+++ components/Avatar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Avatar({url}) {
+  return (
+    <div>
+      <img
+        loading="lazy"
+        className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110"
+        src={url}
+        alt="profile picture"
+      />
+    </div>
+  )
+}
+
+export default Avatar
